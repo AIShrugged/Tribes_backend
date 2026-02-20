5d4a605561ef11c8a6dc7b11aaa4e01f8f4c4e92
onlineheaven <67272832+onlineheaven@users.noreply.github.com>
2026-02-20 14:47:55 +0300

    disable-throttling-on-api-routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,7 +25,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(ThrottleRequests::class);
         $middleware->api(SubstituteBindings::class);
         $middleware->alias([
             'auth' => Authenticate::class,
