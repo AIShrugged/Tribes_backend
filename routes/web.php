6c5f062f294c256f774a431f61e1d47880c5b72c
onlineheaven <67272832+onlineheaven@users.noreply.github.com>
2026-02-25 11:17:34 +0300

    debug-route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dbg', function () {
+    return [
+        'secure' => request()->secure(),
+        'scheme' => request()->getScheme(),
+        'x_forwarded_proto' => request()->header('x-forwarded-proto'),
+        'host' => request()->getHost(),
+    ];
+});
