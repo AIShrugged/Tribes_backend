8b492109f424c5f56a377545092eec7a237dc932
Boris <boris.belov@fit-corp.ru>
2026-04-14 14:46:07 +0300

    test: webhook auto-deploy check

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+// test: webhook auto-deploy check
 abstract class Controller extends \Illuminate\Routing\Controller
 {
     use AuthorizesRequests;
